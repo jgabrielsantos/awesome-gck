54e32561e4dd1103814e35e1c949dde06d465d20
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2023-06-08 22:13:47 -0700

    change main file

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.d\.ts$/,
+        use: 'ignore-loader',
+      },
     ],
   },
 };
